a6c7a10cb09850c10dcd47a1eba30f4e01427303
Ukesh Shrestha <uks56shr567@gmail.com>
2022-04-08 18:10:47 +0545

    Frustrating bugs fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom";
-// import "~@fortawesome/fontawesome-free/css/fontawesome.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 
